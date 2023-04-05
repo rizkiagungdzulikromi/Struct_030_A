0df115cc82a327fc3d5dfd10e8d94dffe252b5ed
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-04-05 16:12:42 +0700

    Add project files.

---
+++ stuktur2/stuktur2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct  Mahasiswa
+{
+	string NIM;
+	string Nama;
+	DetailAlamat Alamat;
+	int uumur;
+};
+
+int main()
+{
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM = ";
+	cin >> mhs.NIM;
+	cout << "Masukkan Nama = ";
+	cin >> mhs.Nama;
+	cout << "Alamat :" << endl;
+	cout << "\tMasukkan Desa = ";
+	cin >> mhs.Alamat.desa;
+	cout << "\Masukkan Kota = ";
+	cin >> mhs.Alamat.kota;
+	cout << "Masukkan Umur = ";
+	cin >> mhs.uumur;
+
+	cout << "\nNIM = " << mhs.NIM;
+	cout << "\nNama = " << mhs.Nama;
+	cout << "\nDesa = " << mhs.Alamat.desa;
+	cout << "\nKota = " << mhs.Alamat.kota;
+	cout << "\nUmur = " << mhs.uumur;
+}
